be90722f45c1337a2519295488ecf0d673e7ba89
terem-andrewc <88650838+terem-andrewc@users.noreply.github.com>
2021-12-03 10:12:00 +1100

    Connection to google apis working

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello world");
+import { google } from "googleapis";
+
+// See https://stackoverflow.com/a/43645891
+const auth = new google.auth.GoogleAuth({
+  keyFile: "./credentials.json",
+  scopes: ["https://www.googleapis.com/auth/userinfo.profile"],
+});
+
+var oauth2 = google.oauth2({
+  auth: auth,
+  version: "v2",
+});
+
+oauth2.userinfo.v2.me.get(function (err, res) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(res);
+  }
+});
