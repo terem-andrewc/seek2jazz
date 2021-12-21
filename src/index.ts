02adba9edcea8be4373140c42aaca2fb0b20f33e
terem-andrewc <88650838+terem-andrewc@users.noreply.github.com>
2021-12-21 19:19:49 +1100

    Added try catch loop

--- src/index.ts
+++ src/index.ts
@@ -45,61 +45,64 @@ async function main() {
       application.fullName,
       application.internalReference
     );
-    const firstname = getFirstname(application.fullName);
-    const lastname = getLastname(application.fullName);
-    let applicantId: string | undefined = await getApplicantIdByNameAndEmail(
-      lastname,
-      application.email
-    );
-    if (!applicantId) {
-      console.log("Creating applicant:", application.fullName);
-      const response = await postApplicant({
-        first_name: firstname,
-        last_name: lastname,
-        email: application.email,
+    try {
+      const firstname = getFirstname(application.fullName);
+      const lastname = getLastname(application.fullName);
+      let applicantId: string | undefined = await getApplicantIdByNameAndEmail(
+        lastname,
+        application.email
+      );
+      if (!applicantId) {
+        console.log("Creating applicant:", application.fullName);
+        const response = await postApplicant({
+          first_name: firstname,
+          last_name: lastname,
+          email: application.email,
+          apikey: process.env.JAZZHR_API_KEY ?? "",
+          phone: application.phone,
+          "base64-resume": application.resume.data,
+        });
+
+        if (response.status !== HttpStatusCode.OK) {
+          throw `Applicant creation failed: ${response.statusText}`;
+        }
+
+        console.log("Applicant created.", response.data.prospect_id);
+        applicantId = response.data.prospect_id;
+
+        if (application.coverLetter) {
+          const postFileResponse = await postFile({
+            applicant_id: applicantId,
+            filename: application.coverLetter.filename,
+            file_data: application.coverLetter.data,
+          });
+          console.log("Cover letter uploaded:", postFileResponse.data);
+        }
+      }
+
+      //look for job
+      const matchingJobs = await getJobsByBoardCode(
+        application.internalReference
+      );
+      if (matchingJobs.length !== 1 || !matchingJobs[0].id) {
+        throw `Internal reference not found: ${application.internalReference}`;
+      }
+      const jobId = matchingJobs[0].id;
+      console.log("Creating job to applicant link:", jobId, applicantId);
+
+      // server does check existance check
+      const applicant2JobPostResponse = await postApplicants2Jobs({
+        applicant_id: applicantId,
+        job_id: jobId,
         apikey: process.env.JAZZHR_API_KEY ?? "",
-        phone: application.phone,
-        "base64-resume": application.resume.data,
       });
 
-      if (response.status !== HttpStatusCode.OK) {
-        throw `Applicant creation failed: ${response.statusText}`;
+      if (applicant2JobPostResponse.status !== HttpStatusCode.OK) {
+        throw `Error while creating applicant2job ${applicant2JobPostResponse}`;
       }
-
-      console.log("Applicant created.", response.data.prospect_id);
-      applicantId = response.data.prospect_id;
-
-      if (application.coverLetter) {
-        const postFileResponse = await postFile({
-          applicant_id: applicantId,
-          filename: application.coverLetter.filename,
-          file_data: application.coverLetter.data,
-        });
-        console.log("Cover letter uploaded:", postFileResponse.data);
-      }
+    } catch (reason) {
+      console.log(reason);
     }
-
-    //look for job
-    const matchingJobs = await getJobsByBoardCode(
-      application.internalReference
-    );
-    if (matchingJobs.length !== 1 || !matchingJobs[0].id) {
-      throw `Internal reference not found: ${application.internalReference}`;
-    }
-    const jobId = matchingJobs[0].id;
-    console.log("Creating job to applicant link:", jobId, applicantId);
-
-    // server does check existance check
-    const applicant2JobPostResponse = await postApplicants2Jobs({
-      applicant_id: applicantId,
-      job_id: jobId,
-      apikey: process.env.JAZZHR_API_KEY ?? "",
-    });
-
-    if (applicant2JobPostResponse.status !== HttpStatusCode.OK) {
-      throw `Error while creating applicant2job ${applicant2JobPostResponse}`;
-    }
-    console.log("Linkage created", applicant2JobPostResponse.data);
   }
 }
 
